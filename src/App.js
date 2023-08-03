aad1a3e0b8e51231357566c90333750f400fbefe
GibaTrindade <gilberto.nunes@tinnova.com.br>
2023-08-03 19:45:05 -0300

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
@@ -14,10 +15,8 @@ function App() {
     event.preventDefault();
     if (guess < secretNumber) {
       setMessage(`Tente um número maior que ${guess}.`);
-      setGuess('')
     } else if (guess > secretNumber) {
       setMessage(`Tente um número menor que ${guess}.`);
-      setGuess('')
     } else {
       setMessage('Parabéns, você acertou!');
       setSecretNumber(Math.floor(Math.random() * 99) + 1);
@@ -26,20 +25,30 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Jogo de Adivinhação</h1>
-      <p>{message}</p>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="number"
-          value={guess}
-          onChange={handleGuessChange}
-          min="1"
-          max="99"
-        />
-        <button type="submit">Enviar</button>
-      </form>
-    </div>
+    <Container className="App">
+      <Row>
+        <Col>
+          <h1 className="text-center">Jogo de Adivinhação</h1>
+          <p className="text-center">{message}</p>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group as={Row} controlId="formGuess">
+              <Col sm="8" className="mx-auto">
+                <Form.Control
+                  type="number"
+                  value={guess}
+                  onChange={handleGuessChange}
+                  min="1"
+                  max="99"
+                />
+              </Col>
+            </Form.Group>
+            <div className="text-center">
+              <Button type="submit">Enviar</Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
