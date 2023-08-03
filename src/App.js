b652d126582de4fa5086892a45babfac655151f4
GibaTrindade <gilberto.nunes@tinnova.com.br>
2023-08-03 19:32:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [secretNumber, setSecretNumber] = useState(Math.floor(Math.random() * 99) + 1);
+  const [guess, setGuess] = useState('');
+  const [message, setMessage] = useState(null);
+
+  const handleGuessChange = (event) => {
+    setGuess(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (guess < secretNumber) {
+      setMessage(`Tente um número maior que ${guess}.`);
+      setGuess('')
+    } else if (guess > secretNumber) {
+      setMessage(`Tente um número menor que ${guess}.`);
+      setGuess('')
+    } else {
+      setMessage('Parabéns, você acertou!');
+      setSecretNumber(Math.floor(Math.random() * 99) + 1);
+      setGuess('');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jogo de Adivinhação</h1>
+      <p>{message}</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          value={guess}
+          onChange={handleGuessChange}
+          min="1"
+          max="99"
+        />
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   );
 }
